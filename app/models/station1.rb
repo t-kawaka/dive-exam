f3e2e26b36f9b7b8e92fff028a6975c058209e83
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-23 06:12:13 +0000

    最寄り駅1駅のみの登録が可能

--- app/models/station1.rb
+++ app/models/station1.rb
@@ -1,4 +1,3 @@
 class Station1 < ApplicationRecord
   belongs_to :estate, optional: true
-  validates :estate_id, presence:true
 end
--- app/models/station2.rb
+++ app/models/station2.rb
@@ -1,4 +1,3 @@
 class Station2 < ApplicationRecord
   belongs_to :estate, optional: true
-  validates :estate_id, presence:true
 end
